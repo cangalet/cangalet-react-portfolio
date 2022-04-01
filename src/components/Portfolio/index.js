3a4105109fbd4d42fd74661f81a17c6a999dece5
Chris <cangalet@gmail.com>
2022-03-31 18:37:32 -0700

    added rendered photos to projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,42 +5,42 @@ export default function Portfolio() {
 
     const portfolio = [
         {
-            img: 'LearnIt-screenshot.png',
+            img: 'https://res.cloudinary.com/dddcuw7qw/image/upload/v1648776426/cangalet-portfolio/LearnIt-screenshot_w7jcvf.png',
             title: 'Learn-IT',
             description: 'Coding Bootcamp Project 2: An interactive educational forum for exploring web development languages!',
             repo: 'https://github.com/ChristopherLawn/learn-code-work',
             live: 'https://damp-journey-96412.herokuapp.com/'
         }, 
         {
-            img: 'StreamView-screenshot.png',
+            img: 'https://res.cloudinary.com/dddcuw7qw/image/upload/v1648776305/cangalet-portfolio/StreamView-screenshot_ukicq7.png',
             title: 'Streamview',
             description: 'Coding Bootcamp Project 1: Web application where an end user is able to search for a movie or tv show and see which streaming service that media is available to view on.',
             repo: 'https://github.com/emdok/bang-imdb',
             live: 'https://emdok.github.io/bang-imdb/'
         }, 
         {
-            img: 'Weather-Dashboard-screenshot.png',
+            img: 'https://res.cloudinary.com/dddcuw7qw/image/upload/v1648776863/cangalet-portfolio/Weather-Dashboard-screenshot_qgqfq5.png',
             title: 'Weather Dashboard',
             description: 'A web app that uses OpenWeather API to view the current weather of your city search along with that cities five day forecast.',
             repo: 'https://github.com/cangalet/CHLG-Weather-Dashboard',
             live: 'https://cangalet.github.io/CHLG-Weather-Dashboard/'
         },
         {
-            img: 'Code-Quiz-screenshot.png',
+            img: 'https://res.cloudinary.com/dddcuw7qw/image/upload/v1648776949/cangalet-portfolio/Code-Quiz-screenshot_wwzofe.png',
             title: 'Code Quiz',
             description: 'Welcome to my code quiz built using JavaScript, HTML, and CSS. Go ahead and see how well you can fair in the alloted time.',
             repo: 'https://github.com/cangalet/CHLG-Code-Quiz',
             live: 'https://cangalet.github.io/CHLG-Code-Quiz/'
         },
         {
-            img: 'StreamView-screenshot',
-            title: 'STREAMVIEW',
+            img: 'https://res.cloudinary.com/dddcuw7qw/image/upload/v1648776745/cangalet-portfolio/Photo-Port-screenshot_sxo9ms.png',
+            title: 'Photo Port',
             description: 'Coding Bootcamp Project 1: Web application where an end user is able to search for a movie or tv show and see which streaming service that media is available to view on.',
-            repo: 'https://github.com/emdok/bang-imdb',
-            live: 'https://emdok.github.io/bang-imdb/'
+            repo: 'https://github.com/cangalet/photo-port',
+            live: 'https://cangalet.github.io/photo-port/'
         },
         {
-            img: 'cangalet-screenshot.png',
+            img: 'https://res.cloudinary.com/dddcuw7qw/image/upload/v1648776269/cangalet-portfolio/cangalet-screenshot_pszzk5.png',
             title: 'Portfolio-V1',
             description: 'Welcome to my portfolio to learn a little more about me. This portfolio is meant to highlight my development skills through this University of Oregon coding bootcamp. Learn more about me, view my projects, and contact me.',
             repo: 'https://github.com/cangalet/chrisangalet',
@@ -58,11 +58,7 @@ export default function Portfolio() {
             >
                 <CardMedia
                 component="img"
-                sx={{
-                    // 16:9
-                    pt: '56.25%',
-                }}
-                image="/static/images/cards/paella.jpg"
+                image={portfolio.img}
                 alt={portfolio.title}
                 />
                 <CardContent sx={{ flexGrow: 1 }}>
